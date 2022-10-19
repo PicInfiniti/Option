f0e114b3a2004f55f18a5df233686517e1832c40
Siavash <siavash.m3070@gmail.com>
2022-10-19 12:58:32 +0330

    add table

--- src/main.js
+++ src/main.js
@@ -1,30 +1,11 @@
 import $ from "jquery"
 import './assets/css/style.css'
-import javascriptLogo from './javascript.svg'
-import {
-  setupCounter
-} from './assets/js/counter.js'
+
+
 
 var server = 'http://192.168.1.11:5000/'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-setupCounter(document.querySelector('#counter'))
+
 
 
 
@@ -35,7 +16,27 @@ $.ajax({
   contentType: "application/json; charset=utf-8",
   dataType: "json",
   success: function (data) {
-    console.log(data)
+    console.log(data.bData);
+    for(let i of data.bData){
+      let tr = $('<tr></tr>').appendTo('#option tbody').append(`
+      <td>${i.val.namad}</td>
+      <td>${i.val.name}</td>
+      <td dir='ltr'>${i.val.pghey}</td>
+      <td dir='ltr'>${i.val.mb}</td>
+      <td dir='ltr'>${i.val.hajm}</td>
+      <td dir='ltr'>${i.val.arzesh}</td>
+      <td dir='ltr'>${i.val.dm}</td>
+      <td dir='ltr'>${i.val.aghey}</td>
+      <td dir='ltr' style='background-color:rgba(197, 197, 245, 0.5); color: black'>${i.val.bt_h}</td>
+      <td dir='ltr' style='background-color:rgba(197, 197, 245, 0.5); color: black'>${i.val.bt_gh}</td>
+      <td dir='ltr' style='background-color:rgba(255, 188, 188, 0.6); color: black'>${i.val.ba_gh}</td>
+      <td dir='ltr' style='background-color:rgba(255, 188, 188, 0.6); color: black'>${i.val.ba_h}</td>
+      <td dir='ltr'>${i.val.ang}</td>
+      <td dir='ltr'>${i.val.r_b}</td>
+      <td dir='ltr'>${i.val.ghe}</td>
+      <td dir='ltr'>${i.val.gh_s_p}</td>
+      `)
+    }
   },
   error: function (errMsg) {
     console.log(errMsg);
